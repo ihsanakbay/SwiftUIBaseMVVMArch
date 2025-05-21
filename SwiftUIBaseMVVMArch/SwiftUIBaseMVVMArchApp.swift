983e1ac0a3e64caa6a2ce0789e11688b5ccb996f
ihsanakbay <ihsanakbay0@gmail.com>
2025-05-21 18:42:40 +0300

    Initial Commit

---
+++ SwiftUIBaseMVVMArch/SwiftUIBaseMVVMArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBaseMVVMArchApp.swift
+//  SwiftUIBaseMVVMArch
+//
+//  Created by İhsan Akbay on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBaseMVVMArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
